34dbefe666d44f8675b972a49980f274dc8e6d04
EmandaHailu <emandahailu65@gmail.com>
2022-11-16 19:38:40 +0300

    studebt profile UI - DONE

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,30 +3,30 @@ import styled from "styled-components";
 import image from "../assets/profile.jpeg";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { cardStyles } from "./ReusableStyles";
-export default function Profile() {
+export default function Profiles() {
   return (
     <Section>
       <div className="image">
         <img src={image} alt="" />
       </div>
       <div className="title">
-        <h2>Kishan Sheth</h2>
+        <h2>Abebe Kebede</h2>
         <h5>
-          <HiOutlineLocationMarker /> New York, USA
+          <HiOutlineLocationMarker /> Addis Ababa, Ethiopia
         </h5>
       </div>
       <div className="info">
         <div className="container">
-          <h5>Days at work</h5>
-          <h3>28</h3>
+          <h5>Completed <br /> Courses</h5>
+          <h3>2</h3>
         </div>
         <div className="container">
-          <h5>Rides</h5>
-          <h3>427</h3>
+          <h5>Ongoing <br /> Courses</h5>
+          <h3>4</h3>
         </div>
         <div className="container">
-          <h5>Hours</h5>
-          <h3>76</h3>
+          <h5>Total <br /> Courses</h5>
+          <h3>6</h3>
         </div>
       </div>
     </Section>
@@ -59,7 +59,7 @@ const Section = styled.section`
     text-align: center;
     h2,
     h5 {
-      color: #ffc107;
+      color: orange;
       font-family: "Permanent Marker", cursive;
       letter-spacing: 0.3rem;
     }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import Profiles from '../components/Profile';
+import { Card, Button, TextInput, InputBase } from '@mantine/core';
 
-function Profile() {
+export default function Profile() {
   return (
-    <div style={{ marginLeft: '19vw' }}>profile</div>
-  )
+    <section style={{ marginLeft: '19vw',marginRight: '1vw', marginTop: '1vw' }}>
+      <Profiles />
+       <br />
+       <Card shadow="xl" p="xl" radius="xl" withBorder>
+        <h3>Edit Profile</h3>
+        <br />
+        <InputBase label="Level of Study" component="select" mt="md">
+        <option value="react">Degree</option>
+        <option value="react">Diploma</option>
+        <option value="svelte">Masters</option>
+      </InputBase>
+        <TextInput
+        label="Level of study"
+        placeholder="Level of study"
+        inputWrapperOrder={['label', 'error', 'input', 'description']}
+      />
+        <TextInput
+        label="Address"
+        placeholder="Address"
+        inputWrapperOrder={['label', 'error', 'input', 'description']}
+      />
+        <TextInput
+        label="Phone Number"
+        placeholder="Phone Number"
+        inputWrapperOrder={['label', 'error', 'input', 'description']}
+      />
+      <Button color="orange" fullWidth mt="md" radius="md" >
+      Update
+    </Button>
+    </Card>
+    <br /><br />
+    </section>
+  );
 }
-
-export default Profile
